047e1e5ed15c626f6f3e2455f0d830b5fad5b541
DELL <DELL@Phanikrishna>
2026-05-28 09:58:37 +0530

    second commit for the code

--- myfirstproject/src/com/collectionframeworks/SecondSmallestIndex.java
+++ myfirstproject/src/com/collectionframeworks/SecondSmallestIndex.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class SecondSmallestIndex {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list=new ArrayList<>(Arrays.asList(5,3,1,7,4,2));
+		ArrayList<Integer> list=new ArrayList<>(Arrays.asList(6,8,1,9,4,2,7,5));
 		
 		int smallest=Integer.MAX_VALUE;
 		int secondSmallest=Integer.MAX_VALUE;
@@ -21,6 +21,7 @@ public class SecondSmallestIndex {
 		}
 		int position=list.indexOf(secondSmallest);
 		
+		System.out.println(smallest);
 		System.out.println("second smallest number: "+secondSmallest);
 		System.out.println("position of the number is: "+position);
 
