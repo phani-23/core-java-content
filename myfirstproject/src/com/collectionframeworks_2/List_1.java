36f1d7d4af88109abf7bff04cf761273b7a80345
DELL <DELL@Phanikrishna>
2026-05-28 23:38:18 +0530

    list interface example program

---
+++ myfirstproject/src/com/collectionframeworks_2/List_1.java
@@ -0,0 +1,44 @@
+package com.collectionframeworks_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_1 {
+
+	public static void main(String[] args) {
+		
+		List<String> l1=new ArrayList<>();
+		l1.add("phani");
+		l1.add(null);
+		l1.add("100");
+		l1.add("");
+		l1.add("g");
+		l1.add("phani");
+		l1.add("g");
+		
+		System.out.println("the printed strings are:");
+		
+		for(String l:l1) {
+			System.out.println(l);
+		}
+
+		l1.set(2, "Krishna");
+		System.out.println(l1);
+		
+		int l2=l1.indexOf("g");
+		System.out.println(l2);
+		
+		int l3=l1.lastIndexOf("g");
+		System.out.println(l3);
+		
+		l1.remove(4);
+		System.out.println(l1);
+		
+		String l5=l1.get(4);
+		System.out.println(l5);
+		
+		boolean b=l1.contains("Krishna");
+		System.out.println(b);
+	}
+
+}
